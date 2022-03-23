eec5215708401bc37d1bb862bc29192be212788f
Lynden Lim <lyndenlim15@gmail.com>
2022-03-23 10:37:59 -0400

    Fetch from twitch api

--- client/src/Homepage.js
+++ client/src/Homepage.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 
 function Homepage() {
+  const [streams, setStreams] = useState([])
+
+  useEffect(() => {
+    axios.get("https://api.twitch.tv/helix/streams?game_id=1469308723&first=1", {
+      headers: {
+        Authorization: "Bearer 6gc86wnfd4e2z6wymi3rzgeczq0ppl",
+        "Client-Id": "yj62jo4k7wcs8xjg6xder4torg8m41"
+      }
+    })
+      .then(res => {
+        setStreams(res.data.data)
+      })
+  }, [])
+
   return (
-    <div>Homepage</div>
+    <div>{streams.map(stream => <Stream key={stream.id} stream={stream} />)}</div>
   )
 }
 
