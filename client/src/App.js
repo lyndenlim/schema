1462a4a8146561b385d3206d4b5487c86764e424
Lynden Lim <lyndenlim15@gmail.com>
2022-03-23 09:35:34 -0400

    Add navbar and homepage components

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,19 @@
-// client/src/components/App.js
 import { useState, useEffect } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import NavBar from "./NavBar";
+import Homepage from "./Homepage";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    fetch("/hello")
-      .then((r) => r.json())
-      .then((data) => setCount(data.count));
-  }, []);
-
   return (
     <BrowserRouter>
       <div className="App">
+        <NavBar />
         <Switch>
           <Route path="/testing">
             <h1>Test Route</h1>
           </Route>
           <Route path="/">
-            <h1>Page Count: {count}</h1>
+            <Homepage/>
           </Route>
         </Switch>
       </div>
---
+++ client/src/NavBar.js
@@ -0,0 +1,10 @@
+import Navbar from "react-bootstrap/Navbar"
+import React from 'react'
+
+function NavBar() {
+  return (
+    <Navbar>Schema</Navbar>
+  )
+}
+
+export default NavBar 
