8da4c8e622997f1578a46f2d127e1493c5f053b4
Lynden Lim <lyndenlim15@gmail.com>
2022-03-23 12:15:35 -0400

    Build stream component

--- client/src/Homepage.js
+++ client/src/Homepage.js
@@ -1,23 +1,31 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
+import Stream from "./Stream"
 
 function Homepage() {
-  const [streams, setStreams] = useState([])
+  const [scienceTechStreams, setScienceTechStreams] = useState([])
+  const [softDevStreams, setSoftDevStreams] = useState([])
 
   useEffect(() => {
-    axios.get("https://api.twitch.tv/helix/streams?game_id=1469308723&first=1", {
+    const axiosInstance = axios.create({
       headers: {
         Authorization: "Bearer 6gc86wnfd4e2z6wymi3rzgeczq0ppl",
         "Client-Id": "yj62jo4k7wcs8xjg6xder4torg8m41"
       }
-    })
+    });
+
+    const scienceAndTechURL = "https://api.twitch.tv/helix/streams?game_id=509670&first=2"
+    const softwareAndDevelopmentURL = "https://api.twitch.tv/helix/streams?game_id=1469308723&first=2"
+
+    axios.all([axiosInstance.get(scienceAndTechURL), axiosInstance.get(softwareAndDevelopmentURL)])
       .then(res => {
-        setStreams(res.data.data)
+        setScienceTechStreams(res[0].data.data)
+        setSoftDevStreams(res[1].data.data)           
       })
   }, [])
 
   return (
-    <div>{streams.map(stream => <Stream key={stream.id} stream={stream} />)}</div>
+    <div>{[...scienceTechStreams, softDevStreams].flat().map(stream => <Stream key={stream.id} stream={stream} />)}</div>
   )
 }
 
