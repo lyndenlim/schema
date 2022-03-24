0d0078847af0d105cdf92727bbecbe96acad4321
Lynden Lim <lyndenlim15@gmail.com>
2022-03-23 20:55:37 -0400

    Component possible fix

---
+++ client/src/SearchBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+//
+function SearchBar() {
+    return (
+        <div>
+            <input placeholder="Search for a video" />
+        </div>
+    )
+}
+
+export default SearchBar
