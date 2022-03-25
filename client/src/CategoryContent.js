46d12a6029772b967373ebe976e485bc9c58e160
Lynden Lim <lyndenlim15@gmail.com>
2022-03-24 21:31:25 -0400

    Add ternary

--- client/src/CategoryContent.js
+++ client/src/CategoryContent.js
@@ -6,7 +6,7 @@ function CategoryContent({ categoryStreams, categoryVideos }) {
   //FIX ISSUE ON REFRESH: on refresh nothing gets rendered...
   return (
     <>
-      <strong>Streams</strong>
+      <strong>{categoryStreams.length > 0 ? "Streams" : ""}</strong>
       {categoryStreams.map(stream => <TwitchThumbnail key={stream.id} stream={stream} />)}
       <strong>Videos</strong>
       {categoryVideos.map(video => <YouTubeThumbnail key={video.etag} video={video} />)}
