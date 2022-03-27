0577698aaec557678e4aac7d89dc23f504af40ca
Lynden Lim <lyndenlim15@gmail.com>
2022-03-27 16:03:41 -0400

    Center YouTube player

--- client/src/Video.js
+++ client/src/Video.js
@@ -4,16 +4,17 @@ import { useParams } from "react-router-dom"
 function Video() {
   const { id } = useParams()
   return (
-    <>
+    <div className="video-player">
       <iframe
         title={id}
         src={`https://www.youtube.com/embed/${id}?enablejsapi=1`}
         width="1150px"
         height="600px"
+        allow="fullscreen"
         frameborder="0"
         style={{ border: "solid 4px #37474F" }}
       ></iframe>
-    </>
+    </div>
   )
 }
 
