1b133dbdd58d98809437ccc69885cb6b3ea83756
Lynden Lim <lyndenlim15@gmail.com>
2022-03-28 17:04:52 -0400

    Serialize technology ID

--- app/serializers/favorite_serializer.rb
+++ app/serializers/favorite_serializer.rb
@@ -1,3 +1,7 @@
 class FavoriteSerializer < ActiveModel::Serializer
   attributes :id, :technology_id, :user_id, :video_id, :twitch_stream_id, :stream_id
+
+  def technology_id
+    self.object.technology.name
+  end
 end
