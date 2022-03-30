9fb4322b5c2b089bdf90330ea9ec07d3f253b186
Lynden Lim <lyndenlim15@gmail.com>
2022-03-29 21:35:49 -0400

    Add thumbnails and styling to schema videos/streams

--- client/src/Homepage.js
+++ client/src/Homepage.js
@@ -5,8 +5,8 @@ import technologies from "./technologies"
 import axios from "axios"
 
 function Homepage({ streams }) {
-  const [playbackIDs, setPlaybackIDs] = useState([])
-  const [videoIDs, setVideoIDs] = useState([])
+  const [streamPlaybackIDs, setStreamPlaybackIDs] = useState([])
+  const [videoPlaybackIDs, setVideoPlaybackIDs] = useState([])
 
   useEffect(async () => {
     const axiosInstance = axios.create({
@@ -18,31 +18,27 @@ function Homepage({ streams }) {
       }
     });
     const res = await axiosInstance.get("https://api.mux.com/video/v1/live-streams")
-    setPlaybackIDs(res.data.data.map(stream => stream.playback_ids[0].id))
+    setStreamPlaybackIDs(res.data.data.map(stream => stream.playback_ids[0].id))
 
     const res2 = await axiosInstance.get("https://api.mux.com/video/v1/assets")
-    setVideoIDs(res2.data.data.map(video => video.id))
+    setVideoPlaybackIDs(res2.data.data.map(video => video.playback_ids[0].id))
   }, [])
-
-  console.log(videoIDs)
 
   return (
     <>
       <div className="stream-header">Schema Streams</div>
       <div className="homepage-container">
-        {playbackIDs.length > 0 ? playbackIDs.map(playbackID => <img key={playbackID} src={"https://png.pngitem.com/pimgs/s/533-5330690_patrick-star-patricio-bobesponja-banda-corridos-patrick-star.png"} alt="stream-image" />) : "No one is currently streaming"}
+        {streamPlaybackIDs.length > 0 ? streamPlaybackIDs.map(playbackID => <div className="mux-thumbnail"><img key={playbackID} width="290px" height="160px" src={"https://png.pngitem.com/pimgs/s/533-5330690_patrick-star-patricio-bobesponja-banda-corridos-patrick-star.png"} alt="stream-image" /></div>) : "No one is currently streaming"}
       </div>
       <div className="stream-header">Schema Videos</div>
       <div className="homepage-container">
-        {videoIDs.length > 0 ? videoIDs.map(videoID => <img key={videoID} src={"https://png.pngitem.com/pimgs/s/533-5330690_patrick-star-patricio-bobesponja-banda-corridos-patrick-star.png"} alt="video-image" />) : "No one is currently streaming"}
+        {videoPlaybackIDs.length > 0 ? videoPlaybackIDs.map(playbackID => <div className="mux-thumbnail"><img key={playbackID} width="290px" height="160px" src={`https://image.mux.com/${playbackID}/thumbnail.png`} /></div>) : "No videos are available at this time"}
       </div>
       <div className="stream-header">Twitch Live Streams</div>
       <div className="homepage-container">
         {streams.map(stream => <TwitchThumbnail key={stream.id} stream={stream} />)}
       </div>
       <br />
-
-
       <div className="homepage-category-container bold">
         {technologies.map(tech => {
           return (
