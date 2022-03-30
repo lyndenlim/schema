b156b5891ddb8242490040aeaab5866d054264e9
Lynden Lim <lyndenlim15@gmail.com>
2022-03-29 22:26:07 -0400

    Style account profile and settings

--- client/src/Homepage.js
+++ client/src/Homepage.js
@@ -7,21 +7,20 @@ import UserVideoThumbnail from './UserVideoThumbnail'
 function Homepage({ streams, streamPlaybackIDs, videoPlaybackIDs }) {
   return (
     <>
-      <div className="stream-header">Schema Streams</div>
+      <h5 className="stream-header">Schema Streams</h5>
       <div className="homepage-container">
         {streamPlaybackIDs.length > 0 ? streamPlaybackIDs.map(playbackID => <div className="mux-thumbnail" key={playbackID}><img width="290px" height="160px" src={`https://image.mux.com/${playbackID}/thumbnail.png`} /></div>) : "No videos are available at this time"}
       </div>
-      <div className="video-header">Schema Videos</div>
+      <h5 className="video-header">Schema Videos</h5>
       <div className="homepage-container">
         {videoPlaybackIDs.length > 0 ? videoPlaybackIDs.map(playbackID => <UserVideoThumbnail key={playbackID} playbackID={playbackID} />) : "No videos are available at this time"}
       </div>
-      <div className="stream-header">Twitch Live Streams</div>
+      <h5 className="stream-header">Twitch Live Streams</h5>
       <div className="homepage-container">
         {streams.map(stream => <TwitchThumbnail key={stream.id} stream={stream} />)}
       </div>
       <br />
-
-      <div className="text-white category-header">Top Categories</div>
+      <h5 className="text-white category-header">Top Categories</h5>
       <div className="homepage-category-container bold">
         {technologies.map(tech => {
           return (
