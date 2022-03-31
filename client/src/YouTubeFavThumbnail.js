5cbebdbc782096ec438932d438e871af8f7ffc0d
Lynden Lim <lyndenlim15@gmail.com>
2022-03-30 23:21:37 -0400

    Name fix

---
+++ client/src/YouTubeFavThumbnail.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom"
+
+function YouTubeFavThumbnail({ favorite }) {
+  const [youtubeTitle, setYoutubeTitle] = useState("");
+  const [channelTitle, setChannelTitle] = useState("");
+  const [videoThumbnail, setVideoThumbnail] = useState("");
+
+  useEffect(async () => {
+    //IF NO IF STATEMENT THIS ERROR OCCURS BUT CODE WORKS:
+    // YouTubeFavThumbnail.js:14 Uncaught (in promise) TypeError: Cannot read properties of undefined (reading 'snippet')
+    const res4 = await axios.get(
+      `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&id=${favorite.video_id}&maxResults=1&key=AIzaSyAP_e37kjSD1mbasiA3YoA24_y14uaDBgU`
+    );
+    setYoutubeTitle(res4.data.items[0].snippet.localized.title);
+    setChannelTitle(res4.data.items[0].snippet.channelTitle);
+    setVideoThumbnail(res4.data.items[0].snippet.thumbnails.high.url);
+
+  }, [])
+
+  return (
+    <Link to={`/videos/${favorite.video_id}`} style={{ textDecoration: "none" }}>
+      <div className="favorite-thumbnail">
+        {favorite.video_id ? <img
+          width="290px"
+          height="200px"
+          src={videoThumbnail}
+          alt="video-thumbnail"
+        /> : null}
+        <p className="text-white">
+          {favorite.video_id ? <strong>
+            {youtubeTitle.length < 25
+              ? youtubeTitle
+              : `${youtubeTitle.slice(0, 25)}...`}
+          </strong> : null}
+        </p>
+        <p className="text-white">{favorite.video_id ? channelTitle : null}</p>
+      </div>
+    </Link>
+  );
+}
+
+export default YouTubeFavThumbnail;
