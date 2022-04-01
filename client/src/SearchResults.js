b2d3e0299ec2d734b3396832a34984dc39dad158
Lynden Lim <lyndenlim15@gmail.com>
2022-04-01 11:38:59 -0400

    Add padding to search result page

--- client/src/SearchResults.js
+++ client/src/SearchResults.js
@@ -6,7 +6,7 @@ function SearchResults({ searchedStreams, searchedVideos }) {
   return (
     <div style={{paddingTop: "100px"}}>
       <div className="centered">
-        {searchedStreams.length > 0 ? <h4 className="bold text-white">Streams</h4> : <div><h4 className="bold text-white">Streams</h4><h6 className="text-white">No one is currently streaming</h6></div>}
+        {searchedStreams.length > 0 ? <h4 className="bold text-white">Streams</h4> : <div><h4 className="bold text-white">Streams</h4><h6 className="text-white none-streaming">No one is currently streaming</h6></div>}
       </div>
       <div className="homepage-container">
         {searchedStreams.map(stream => <TwitchThumbnail key={stream.id} stream={stream} />)}
